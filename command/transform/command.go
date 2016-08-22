695fda80bd25b3c8f1eb2bdf592b4a7e1111821b
Timm Friebe <thekid@thekid.de>
2016-08-23 00:36:01 +0200

    Refactor: Move parsing to transformation

--- command/transform/command.go
+++ command/transform/command.go
@@ -9,7 +9,6 @@ import (
 	"github.com/tueftler/doget/dockerfile"
 	"io"
 	"os"
-	"path/filepath"
 )
 
 type TransformCommand struct {
@@ -24,26 +23,6 @@ var (
 // Creates new transform command instance
 func NewCommand(name string) *TransformCommand {
 	return &TransformCommand{flags: flag.NewFlagSet(name, flag.ExitOnError)}
-}
-
-func parse(parser *dockerfile.Parser, input string, file *dockerfile.Dockerfile) error {
-	stat, err := os.Stat(input)
-	if err != nil {
-		return err
-	}
-
-	if stat.IsDir() {
-		for _, name := range variants {
-			variant := filepath.Join(input, name)
-			_, err := os.Stat(variant)
-			if err == nil {
-				return parser.ParseFile(variant, file)
-			}
-		}
-		return fmt.Errorf("Neither Dockerfile.in or Dockerfile exist in %s", input)
-	} else {
-		return parser.ParseFile(input, file)
-	}
 }
 
 func open(output string) (io.Writer, error) {
@@ -65,12 +44,6 @@ func (c *TransformCommand) Run(config *config.Configuration, parser *dockerfile.
 
 	fmt.Fprintf(os.Stderr, "> Running transform(%q -> %q) using %s\n", input, output, config.Source)
 
-	// Parse input
-	var file dockerfile.Dockerfile
-	if err := parse(parser, input, &file); err != nil {
-		return err
-	}
-
 	// Open output
 	out, err := open(output)
 	if err != nil {
@@ -79,9 +52,8 @@ func (c *TransformCommand) Run(config *config.Configuration, parser *dockerfile.
 
 	// Transform
 	var buf bytes.Buffer
-	transformation := Transformation{Output: &buf}
-	transformation.Instruction("FROM", file.From.Image)
-	if err := transformation.Write(config, parser, &file, ""); err != nil {
+	transformation := Transformation{Input: input, Output: &buf}
+	if err := transformation.Run(config, parser); err != nil {
 		return err
 	}
 	fmt.Fprintf(os.Stderr, "Done\n\n")
--- command/transform/transformation.go
+++ command/transform/transformation.go
@@ -13,18 +13,50 @@ import (
 )
 
 type Transformation struct {
+	Input  string
 	Output io.Writer
 }
 
-func (t *Transformation) Comment(value string) {
+func parse(parser *dockerfile.Parser, input string, file *dockerfile.Dockerfile) error {
+	stat, err := os.Stat(input)
+	if err != nil {
+		return err
+	}
+
+	if stat.IsDir() {
+		for _, name := range variants {
+			variant := filepath.Join(input, name)
+			_, err := os.Stat(variant)
+			if err == nil {
+				return parser.ParseFile(variant, file)
+			}
+		}
+		return fmt.Errorf("Neither Dockerfile.in or Dockerfile exist in %s", input)
+	} else {
+		return parser.ParseFile(input, file)
+	}
+}
+
+// Run transformation
+func (t *Transformation) Run(config *config.Configuration, parser *dockerfile.Parser) error {
+	var file dockerfile.Dockerfile
+	if err := parse(parser, t.Input, &file); err != nil {
+		return err
+	}
+
+	t.instruction("FROM", file.From.Image)
+	return t.write(config, parser, &file, "")
+}
+
+func (t *Transformation) comment(value string) {
 	fmt.Fprintf(t.Output, "# %s\n", strings.Replace(value, "\n", "\n# ", -1))
 }
 
-func (t *Transformation) Instruction(instruction, value string) {
+func (t *Transformation) instruction(instruction, value string) {
 	fmt.Fprintf(t.Output, "%s %s\n\n", instruction, strings.Replace(value, "\n", "\\\n", -1))
 }
 
-func (t *Transformation) Write(config *config.Configuration, parser *dockerfile.Parser, file *dockerfile.Dockerfile, base string) error {
+func (t *Transformation) write(config *config.Configuration, parser *dockerfile.Parser, file *dockerfile.Dockerfile, base string) error {
 	for _, statement := range file.Statements {
 		switch statement.(type) {
 		case *use.Statement:
@@ -63,66 +95,66 @@ func (t *Transformation) Write(config *config.Configuration, parser *dockerfile.
 				)
 			}
 
-			t.Comment("Included from " + reference)
-			t.Write(config, parser, &included, filepath.ToSlash(path)+"/")
+			t.comment("Included from " + reference)
+			t.write(config, parser, &included, filepath.ToSlash(path)+"/")
 			break
 
 		// Retain comments
 		case *dockerfile.Comment:
-			t.Comment(statement.(*dockerfile.Comment).Lines)
+			t.comment(statement.(*dockerfile.Comment).Lines)
 			break
 
 		// Builtin Docker instructions
 		case *dockerfile.Maintainer:
-			t.Instruction("MAINTAINER", statement.(*dockerfile.Maintainer).Name)
+			t.instruction("MAINTAINER", statement.(*dockerfile.Maintainer).Name)
 			break
 		case *dockerfile.Run:
-			t.Instruction("RUN", statement.(*dockerfile.Run).Command)
+			t.instruction("RUN", statement.(*dockerfile.Run).Command)
 			break
 		case *dockerfile.Label:
-			t.Instruction("LABEL", statement.(*dockerfile.Label).Pairs)
+			t.instruction("LABEL", statement.(*dockerfile.Label).Pairs)
 			break
 		case *dockerfile.Expose:
-			t.Instruction("EXPOSE", statement.(*dockerfile.Expose).Ports)
+			t.instruction("EXPOSE", statement.(*dockerfile.Expose).Ports)
 			break
 		case *dockerfile.Env:
-			t.Instruction("ENV", statement.(*dockerfile.Env).Pairs)
+			t.instruction("ENV", statement.(*dockerfile.Env).Pairs)
 			break
 		case *dockerfile.Add:
-			t.Instruction("ADD", base+statement.(*dockerfile.Add).Paths)
+			t.instruction("ADD", base+statement.(*dockerfile.Add).Paths)
 			break
 		case *dockerfile.Copy:
-			t.Instruction("COPY", base+statement.(*dockerfile.Copy).Paths)
+			t.instruction("COPY", base+statement.(*dockerfile.Copy).Paths)
 			break
 		case *dockerfile.Entrypoint:
-			t.Instruction("ENTRYPOINT", statement.(*dockerfile.Entrypoint).CmdLine)
+			t.instruction("ENTRYPOINT", statement.(*dockerfile.Entrypoint).CmdLine)
 			break
 		case *dockerfile.Volume:
-			t.Instruction("VOLUME", statement.(*dockerfile.Volume).Names)
+			t.instruction("VOLUME", statement.(*dockerfile.Volume).Names)
 			break
 		case *dockerfile.User:
-			t.Instruction("USER", statement.(*dockerfile.User).Name)
+			t.instruction("USER", statement.(*dockerfile.User).Name)
 			break
 		case *dockerfile.Workdir:
-			t.Instruction("WORKDIR", statement.(*dockerfile.Workdir).Path)
+			t.instruction("WORKDIR", statement.(*dockerfile.Workdir).Path)
 			break
 		case *dockerfile.Arg:
-			t.Instruction("ARG", statement.(*dockerfile.Arg).Name)
+			t.instruction("ARG", statement.(*dockerfile.Arg).Name)
 			break
 		case *dockerfile.Onbuild:
-			t.Instruction("ONBUILD", statement.(*dockerfile.Onbuild).Instruction)
+			t.instruction("ONBUILD", statement.(*dockerfile.Onbuild).Instruction)
 			break
 		case *dockerfile.Stopsignal:
-			t.Instruction("STOPSIGNAL", statement.(*dockerfile.Stopsignal).Signal)
+			t.instruction("STOPSIGNAL", statement.(*dockerfile.Stopsignal).Signal)
 			break
 		case *dockerfile.Healthcheck:
-			t.Instruction("HEALTHCHECK", statement.(*dockerfile.Healthcheck).Command)
+			t.instruction("HEALTHCHECK", statement.(*dockerfile.Healthcheck).Command)
 			break
 		case *dockerfile.Shell:
-			t.Instruction("SHELL", statement.(*dockerfile.Shell).CmdLine)
+			t.instruction("SHELL", statement.(*dockerfile.Shell).CmdLine)
 			break
 		case *dockerfile.Cmd:
-			t.Instruction("CMD", statement.(*dockerfile.Cmd).CmdLine)
+			t.instruction("CMD", statement.(*dockerfile.Cmd).CmdLine)
 			break
 		}
 	}
