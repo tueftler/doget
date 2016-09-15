2bdeec3cb48b013ea7306d73583a4d612bc09a03
Frank Kleine <frank.kleine@1und1.de>
2016-09-15 13:21:30 +0200

    add some more tests

--- command/build/command_test.go
+++ command/build/command_test.go
@@ -16,7 +16,17 @@ func Test_splitArgsRecognizesTransformArgs(t *testing.T) {
 	assertEqual([]string{"-no-cache", "true"}, transformArgs, t)
 }
 
+func Test_splitArgsRecognizesTransformArgWithoutValue(t *testing.T) {
+	transformArgs, _ := split([]string{"-t", "foo:bar", "--doget-clean", "--no-cache=true"})
+	assertEqual([]string{"-clean"}, transformArgs, t)
+}
+
 func Test_splitArgsRecognizesDockerBuildArgs(t *testing.T) {
-	_, dockerArgs := split([]string{"--doget-no-cache=true", "-t", "foo:bar", "--no-cache=true"})
-	assertEqual([]string{"build", "-t", "foo:bar", "--no-cache=true"}, dockerArgs, t)
+	_, dockerArgs := split([]string{"-t", "foo:bar", "--no-cache=true", "--doget-no-cache=true", "."})
+	assertEqual([]string{"build", "-t", "foo:bar", "--no-cache=true", "."}, dockerArgs, t)
+}
+
+func Test_dockerArgsContainAtLeastBuild(t *testing.T) {
+	_, dockerArgs := split([]string{})
+	assertEqual([]string{"build"}, dockerArgs, t)
 }
