9717e9df8fe76c981f166530db250dcd94e72fd3
Timm Friebe <thekid@thekid.de>
2016-09-16 19:15:52 +0200

    Fix tagging

--- push.sh
+++ push.sh
@@ -3,19 +3,21 @@
 branch=${1-none}
 tag=${2-}
 
-if [ "master" != $branch ] ; then
+if [ "" != "$tag" ] && [ ${tag:1:1} -ge 1 ]; then
+  version=${tag:0:2}
+elif [ "master" != $branch ] ; then
   echo "Not pushing branch $branch"
   exit 0
+else
+  version=
 fi
 
 echo "Pushing :latest"
 docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" ;
 docker push tueftler/doget ;
 
-if [ "" != "$tag" ] && [ ${tag:1:1} -ge 1 ]; then
-  tag=${tag:0:2};
-
-  echo "Pushing :$tag"
-  docker tag tueftler/doget:$tag tueftler/doget:latest;
-  docker push tueftler/doget:$tag;
+if [ "" != "$version" ] ; then
+  echo "Pushing :$version"
+  docker tag tueftler/doget:$version tueftler/doget:latest;
+  docker push tueftler/doget:$version;
 fi
